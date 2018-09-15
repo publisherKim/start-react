7acfd25fc41e1032158a9d2d02f18f0e7a0d9cd9
incorrectDeveloper <hdpgpp@gmail.com>
2018-09-15 09:56:19 +0900

    component 순회 돔 만들기

---
+++ src/IterationSample.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class IterationSample extends Component {
+  render() {
+    const names = ['눈사람', '얼음', '눈', '바람'];
+    const nameList = names.map(
+      name => <li>{name}</li>
+    );
+    return (
+      <div>
+        {nameList}
+      </div>
+    );
+  }
+}
+
+export default IterationSample;
