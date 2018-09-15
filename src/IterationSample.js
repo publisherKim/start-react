7d26f6df7f625665f3bb1862cc02ab00f6773cd3
incorrectDeveloper <hdpgpp@gmail.com>
2018-09-15 10:19:58 +0900

    챕터6-3 data추가

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 
 class IterationSample extends Component {
+  state = {
+    names: ['눈사람', '얼음', '눈', '바람'],
+    name: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleInsert = () => {
+    this.setState({
+      names: this.state.names.concat(this.state.name),
+      name: ''
+    })
+  }
+
   render() {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map(
+    console.log(this.state.names);
+    const nameList = this.state.names.map(
       (name, index) => <li key={index}>{name}</li>
     );
-    console.log('nameList', nameList);
-    // 호 그냥 자바스크립트 맵의 배열의 반환값이랑은 다름 어떤 처리를 해준다는걸 알수있음 key나 react자체의 돔 처리등...
     return (
       <div>
-        {nameList}
+        <input
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <button onClick={this.handleInsert}>추가</button> 
+        <ul>
+          {nameList}
+        </ul>
       </div>
-      // 키가 없으면 경고 메세지 키가 있을 경우에 데이터와 돔간의 매치 등에서 훨씬 빠름
     );
   }
 }
